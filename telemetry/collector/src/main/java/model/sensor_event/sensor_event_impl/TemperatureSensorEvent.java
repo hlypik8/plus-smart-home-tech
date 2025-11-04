9beca62d818fda63c6125a90382c911b9f25fee5
hlypik <arsenyborovskoi@yandex.ru>
2025-11-04 16:19:09 +0300

    добавленаы схемы Avro и модели

---
+++ telemetry/collector/src/main/java/model/sensor_event/sensor_event_impl/TemperatureSensorEvent.java
@@ -0,0 +1,4 @@
+package model.sensor_event.sensor_event_impl;
+
+public class TemperatureSensorEvent {
+}
---
+++ telemetry/serialization/avro-schemas/src/main/java/AvroSerializer.java
@@ -0,0 +1,30 @@
+import org.apache.avro.io.BinaryEncoder;
+import org.apache.avro.io.DatumWriter;
+import org.apache.avro.io.EncoderFactory;
+import org.apache.avro.specific.SpecificDatumWriter;
+import org.apache.avro.specific.SpecificRecordBase;
+import org.apache.kafka.common.errors.SerializationException;
+import org.apache.kafka.common.serialization.Serializer;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
+public class AvroSerializer implements Serializer<SpecificRecordBase> {
+
+    private final EncoderFactory encoderFactory = EncoderFactory.get();
+    private BinaryEncoder encoder;
+
+    public byte[] serialize(String topic, SpecificRecordBase data){
+        if (data == null) return null;
+
+        try (ByteArrayOutputStream output = new ByteArrayOutputStream()) {
+            DatumWriter<SpecificRecordBase> writer = new SpecificDatumWriter<>(data.getSchema());
+            encoder = encoderFactory.binaryEncoder(output, encoder);
+            writer.write(data, encoder);
+            encoder.flush();
+            return output.toByteArray();
+        } catch (IOException e){
+            throw new SerializationException("Ошибка сериализации данных", e);
+        }
+    }
+}
