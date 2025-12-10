30e95f661479c50d06b31a1869067cab0cfc7f98
hlypik <arsenyborovskoi@yandex.ru>
2025-12-10 13:00:04 +0300

    фикс мапперов

--- commerce/shopping-cart/src/main/java/cart/CartMapper.java
+++ commerce/shopping-cart/src/main/java/cart/CartMapper.java
@@ -3,11 +3,22 @@ package cart;
 import interaction_api.cart.model.ShoppingCartDto;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
 import org.mapstruct.ReportingPolicy;
 
 @Mapper(componentModel = "spring",
         unmappedTargetPolicy = ReportingPolicy.ERROR)
 public interface CartMapper {
+
     @Mapping(source = "cartId", target = "shoppingCartId")
+    @Mapping(source = "products", target = "products")
     ShoppingCartDto toDto(Cart cart);
+
+    @Mapping(source = "shoppingCartId", target = "cartId")
+    @Mapping(source = "products", target = "products")
+    Cart toEntity(ShoppingCartDto shoppingCartDto);
+
+    @Mapping(source = "shoppingCartId", target = "cartId")
+    @Mapping(source = "products", target = "products")
+    void updateFromDto(@MappingTarget Cart cart, ShoppingCartDto shoppingCartDto);
 }
--- commerce/warehouse/src/main/java/warehouse/WarehouseMapper.java
+++ commerce/warehouse/src/main/java/warehouse/WarehouseMapper.java
@@ -3,14 +3,21 @@ package warehouse;
 import interaction_api.warehouse.model.NewProductInWarehouseRequest;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
 import org.mapstruct.ReportingPolicy;
 
 @Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.ERROR)
 public interface WarehouseMapper {
 
-    @Mapping(target = "width", source = "product.dimensions.width")
-    @Mapping(target = "height", source = "product.dimensions.height")
-    @Mapping(target = "depth", source = "product.dimensions.depth")
+    @Mapping(target = "width", source = "dimensions.width")
+    @Mapping(target = "height", source = "dimensions.height")
+    @Mapping(target = "depth", source = "dimensions.depth")
     @Mapping(target = "quantity", ignore = true)
     WarehouseProduct toWarehouseProduct(NewProductInWarehouseRequest product);
+
+    @Mapping(target = "width", source = "dimensions.width")
+    @Mapping(target = "height", source = "dimensions.height")
+    @Mapping(target = "depth", source = "dimensions.depth")
+    @Mapping(target = "quantity", ignore = true)
+    void updateFromDto(@MappingTarget WarehouseProduct warehouseProduct, NewProductInWarehouseRequest product);
 }
