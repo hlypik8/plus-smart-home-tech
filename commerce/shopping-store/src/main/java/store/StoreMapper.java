fbd48fdb0a6264469ae5194aef1ab5c4c329a2df
hlypik <arsenyborovskoi@yandex.ru>
2025-12-10 12:51:00 +0300

    фикс маппера

--- commerce/shopping-store/src/main/java/store/StoreMapper.java
+++ commerce/shopping-store/src/main/java/store/StoreMapper.java
@@ -10,11 +10,31 @@ import org.mapstruct.ReportingPolicy;
 public interface StoreMapper {
 
     @Mapping(source = "name", target = "productName")
+    @Mapping(source = "productId", target = "productId")
+    @Mapping(source = "description", target = "description")
+    @Mapping(source = "imageSrc", target = "imageSrc")
+    @Mapping(source = "quantityState", target = "quantityState")
+    @Mapping(source = "productState", target = "productState")
+    @Mapping(source = "productCategory", target = "productCategory")
+    @Mapping(source = "price", target = "price")
     ProductDto toProductDto(StoreProduct storeProduct);
 
     @Mapping(source = "productName", target = "name")
+    @Mapping(source = "productId", target = "productId")
+    @Mapping(source = "description", target = "description")
+    @Mapping(source = "imageSrc", target = "imageSrc")
+    @Mapping(source = "quantityState", target = "quantityState")
+    @Mapping(source = "productState", target = "productState")
+    @Mapping(source = "productCategory", target = "productCategory")
+    @Mapping(source = "price", target = "price")
     StoreProduct toStoreProduct(ProductDto productDto);
 
     @Mapping(source = "productName", target = "name")
+    @Mapping(source = "description", target = "description")
+    @Mapping(source = "imageSrc", target = "imageSrc")
+    @Mapping(source = "quantityState", target = "quantityState")
+    @Mapping(source = "productState", target = "productState")
+    @Mapping(source = "productCategory", target = "productCategory")
+    @Mapping(source = "price", target = "price")
     void updateFromDto(@MappingTarget StoreProduct storeProduct, ProductDto productDto);
 }
